e591958a096250b29c678e58f44f4262c4e8d1c0
akangRio <fachryo97@gmail.com>
2023-05-30 20:28:33 +0700

    feat: using error handler middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,11 +5,14 @@ const jobRoute = require("./jobRoute");
 const employerRoute = require("./employerRoute");
 const adminRoute = require("./adminRoute");
 const loginRoute = require("./loginRoute");
+const errorHandler = require("../middleware/errorHandler");
 
 router.use("/user", userRoute);
 router.use("/job", jobRoute);
 router.use("/employer", employerRoute);
 router.use("/admin", adminRoute);
 router.use("/login", loginRoute);
+
+router.use(errorHandler);
 
 module.exports = router;
