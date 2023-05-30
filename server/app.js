bfe1b21e60da790d5f066eab23323669d3306373
fawzanrizqy <fawzan.rizqy29@gmail.com>
2023-05-30 15:52:37 +0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const cors = require('cors')
+const app = express()
+const port = 3000 //process.env.PORT || 3000;
+//const route = require('./routes/route');
+
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//app.use('/', route);
+//route example:
+//app.post('/register', Controller.register)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port port`)
+})
