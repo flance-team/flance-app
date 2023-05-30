d5b30d88b25011f51797b02ddb6a9668584809d5
fawzanrizqy <fawzan.rizqy29@gmail.com>
2023-05-30 15:54:13 +0700

    first commit dev

--- server/app.js
+++ server/app.js
@@ -5,8 +5,8 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const cors = require('cors')
 const app = express()
-const port = 3000 //process.env.PORT || 3000;
-//const route = require('./routes/route');
+const port = process.env.PORT || 3000;
+// const route = require('./routes/route');
 
 
 app.use(cors());
