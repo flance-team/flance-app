3ca151c28044434dccd88184e874a9d1841acb61
fawzanrizqy <fawzan.rizqy29@gmail.com>
2023-06-01 21:18:03 +0700

    feat:payment done

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -196,6 +196,26 @@ class jobController {
     static async payUser(req, res, next) {
         try {
 
+            const employerId = +req.identity.id;
+            const { userId, amount } = req.body;
+
+            const depositEmp = await DepositEmployer.findOne({ where: { employerId: employerId } });
+            if (!depositEmp) {
+                throw ({ name: "not_found", message: "Deposit Employer not found.", code: 404 })
+            }
+            const depositUser = await DepositUser.findOne({ where: { userId: userId } });
+            if (!depositUser) {
+                throw ({ name: "not_found", message: "Deposit User not found.", code: 404 })
+            }
+
+            const transEmp = await TransactionEmployer.create({ depositId: depositEmp.id, amount: amount * -1, ref: `pay - user ${userId}`, transactionDate: new Date(), updatedBalance: depositEmp.balance - amount });
+            await depositEmp.update({ balance: depositEmp.balance - amount });
+
+            const transUser = await TransactionUser.create({ depositId: depositUser.id, amount: amount, ref: `payment from - employer ${employerId}`, transactionDate: new Date(), updatedBalance: depositUser.balance + amount });
+            await depositUser.update({ balance: depositUser.balance + amount });
+
+            res.status(201).json(transEmp);
+
         } catch (err) {
             next(err);
         }
@@ -203,7 +223,18 @@ class jobController {
 
     static async withdrawUser(req, res, next) {
         try {
+            const idUser = +req.identity.id;
+            const { amount } = req.body;
 
+            const depositUser = await DepositUser.findOne({ where: { userId: idUser } });
+            if (!depositUser) {
+                throw ({ name: "not_found", message: "Deposit User not found.", code: 404 })
+            }
+
+            const transUser = await TransactionUser.create({ depositId: depositUser.id, amount: amount * -1, ref: `withdraw - user ${idUser}`, transactionDate: new Date(), updatedBalance: depositUser.balance - amount });
+            await depositUser.update({ balance: depositUser.balance - amount });
+
+            res.status(201).json(transUser);
         } catch (err) {
             next(err);
         }
