94cfe69f9ae006fa1fbfe1f9091e4e28cc3582bb
akangRio <fachryo97@gmail.com>
2023-06-08 02:28:14 +0700

    hello

--- client-app/src/components/NavbarEmployer.jsx
+++ client-app/src/components/NavbarEmployer.jsx
@@ -18,6 +18,7 @@ const NavbarEmployer = () => {
 
   const base_url_server = "http://localhost:3000";
   const [balance, setBalance] = useState();
+  const [image, setImage] = useState("");
 
   function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
@@ -39,6 +40,7 @@ const NavbarEmployer = () => {
         currency: "IDR",
       });
       setBalance(formattedAmount);
+      setImage(response.data.Employer.imgUrl);
     } catch (err) {
       console.log(err);
     }
@@ -168,7 +170,7 @@ const NavbarEmployer = () => {
                           <span className="sr-only">Open user menu</span>
                           <img
                             className="h-8 w-8 rounded-full"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            src={image}
                             alt=""
                           />
                         </Menu.Button>
