3b82c6a84e9b3eef25bdf29cd74c457156c7a417
akangRio <fachryo97@gmail.com>
2023-06-08 00:48:19 +0700

    feat: modal

--- client-app/src/app/EmployerListEmployee/page.jsx
+++ client-app/src/app/EmployerListEmployee/page.jsx
@@ -6,6 +6,7 @@ import Loading from "../../components/Loading";
 import { Dialog, Transition } from "@headlessui/react";
 import CurrencyInput from "react-currency-input-field";
 import { useRouter } from "next/navigation";
+import Swal from "sweetalert2";
 
 const base_url_server = "http://localhost:3000";
 
@@ -86,6 +87,10 @@ const EmployerListEmployee = () => {
       );
       console.log(response);
       closeModal();
+      Swal.fire({
+        icon: "success",
+        title: "Employee has been Paid",
+      });
     } catch (err) {
       console.log(err);
     }
@@ -97,66 +102,99 @@ const EmployerListEmployee = () => {
 
   return (
     <>
-      <NavbarEmployer />
-      <div className="bg-white min-h-screen flex flex-col mx-7 my-2 mt-3">
-        <header className="bg-white shadow">{/* Header content */}</header>
-        <div className="flex flex-grow">
-          {/* CARD SEBELAH KIRI */}
-          <aside className="bg-white w-64">
-            <div className="card w-56 bg-base-100 shadow-xl items-center">
-              <div className="w-32 h-32 flex justify-center rounded-full overflow-hidden">
-                <img
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt="Profile Image"
-                />
-              </div>
-              <div className="card-body text-center items-center">
-                <h2 className="card-title text-xl font-semibold place-items-center">
-                  {nameUser}
-                </h2>
-              </div>
-            </div>
-          </aside>
-
-          <div className="flex-grow">
-            <div className="flex items-center justify-between mb-4">
-              <h2 className="text-2xl font-semibold">Employee List</h2>
-            </div>
-            <div className="flex bg-white rounded-lg flex-col items-center space-y-2">
-              <table className="w-full border">
+      <div className="min-h-full">
+        {/* INI BUNGKUS BIRU FLEX KOLOM */}
+        <div className="flex flex-1 flex-col">
+          <NavbarEmployer />
+          {/* INI BUNGKUS YANG KUNING BIKIN JADI DI TENGAH, HEIGHTNYA TERGANTUNG ISI DARI ELEMENTNYA BERAPA MENYESUAIKAN h-fit */}
+          <main className="flex-1 pb-8 w-3/4 m-auto h-fit">
+            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <table className="w-full divide-y divide-gray-300">
                 <thead>
                   <tr>
-                    <th className="border">Name</th>
-                    <th className="border">Job Title</th>
-                    <th className="border">Total Hours</th>
-                    <th className="border">Rate Per Hour</th>
-                    <th className="border">Action</th>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Job Title
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Total Hours
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Rate Per Hour
+                    </th>
+                    <th
+                      scope="col"
+                      className="relative py-3.5 pl-3 pr-4 sm:pr-0"
+                    >
+                      <span className="sr-only">Pay</span>
+                    </th>
                   </tr>
                 </thead>
-                <tbody>
-                  {employees.map((employee) => (
-                    <tr key={employee.id}>
-                      <td className="border text-center">
-                        {employee.User.name}
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {employees.map((employee, i) => (
+                    <tr key={i}>
+                      <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                        <div className="flex items-center">
+                          <div className="h-11 w-11 flex-shrink-0">
+                            <img
+                              className="h-11 w-11 rounded-full"
+                              src={employee?.User?.imgUrl}
+                              alt="https://static.vecteezy.com/system/resources/previews/007/296/443/original/user-icon-person-icon-client-symbol-profile-icon-vector.jpg"
+                            />
+                          </div>
+                          <div className="ml-4">
+                            <div className="font-medium text-gray-900">
+                              {employee?.User?.name}
+                            </div>
+                            <div className="mt-1 text-gray-500">
+                              {employee?.User?.email}
+                            </div>
+                          </div>
+                        </div>
                       </td>
-                      <td className="border text-center">
-                        {employee.Job.title}
+                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                        <div className="text-gray-900">
+                          {employee?.Job?.title}
+                        </div>
+                        <div className="mt-1 text-gray-500">
+                          {/* {employee?.User?.gender} */}
+                        </div>
                       </td>
-                      <td className="border text-center">
-                        {employee.totalHours}
+                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                        {/* <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                          Active
+                        </span> */}
+                        {employee?.totalHours < 1
+                          ? employee?.totalHours + " hour"
+                          : employee?.totalHours + " hours"}
                       </td>
-                      <td className="border text-center">
-                        {employee.totalSalary.toLocaleString("id-ID", {
+                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                        {employee?.Job?.salary.toLocaleString("id-ID", {
                           style: "currency",
                           currency: "IDR",
-                        })}
+                        }) + "/hour"}
                       </td>
-                      <td className="border text-center">
+                      <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                         <button
-                          className="btn btn-primary m-2"
+                          className="text-indigo-600 hover:text-indigo-900"
                           onClick={() => handlePay(employee)}
                         >
                           Pay
+                          <span className="sr-only">, {employee.name}</span>
                         </button>
                       </td>
                     </tr>
@@ -164,9 +202,8 @@ const EmployerListEmployee = () => {
                 </tbody>
               </table>
             </div>
-          </div>
+          </main>
         </div>
-        <footer className="bg-white shadow"></footer>
       </div>
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
@@ -193,7 +230,7 @@ const EmployerListEmployee = () => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-screen transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-fit transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
@@ -201,10 +238,16 @@ const EmployerListEmployee = () => {
                     Wage Payment Summary
                   </Dialog.Title>
                   <div className="mt-2 flex">
-                    <div className="w-1/4">
-                      <div className="avatar">
+                    <div className="w-1/3 m-2">
+                      <div className="avatar mb-2">
                         <div className="w-32 rounded">
-                          <img src="https://images.unsplash.com/photo-1595475038784-bbe439ff41e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJhcmlzdGF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60" />
+                          <img
+                            src={
+                              chosenUser?.User?.imgUrl
+                                ? chosenUser?.User?.imgUrl
+                                : "https://static.vecteezy.com/system/resources/previews/007/296/443/original/user-icon-person-icon-client-symbol-profile-icon-vector.jpg"
+                            }
+                          />
                         </div>
                       </div>
                       <p className="text-sm text-black font-medium">
@@ -227,94 +270,89 @@ const EmployerListEmployee = () => {
                       </p>
                     </div>
                     <div className="divider divider-horizontal"></div>
-                    <div className=" w-full">
+                    <div className="w-full">
                       <div className="bg-base-200 rounded-md flex justify-center">
                         <h2 className="text-lg font-medium ml-2">
                           Job Contract Info
                         </h2>
                       </div>
-                      <div className="flex">
-                        <h2 className="text-lg font-medium ml-2">
-                          Rate Per Hour Fee:
-                        </h2>
-                        <p className="self-center ml-2">
-                          {chosenUser?.Job?.salary.toLocaleString("id-ID", {
-                            style: "currency",
-                            currency: "IDR",
-                          })}
-                        </p>
-                      </div>
-                      <div className="flex">
-                        <h2 className="text-lg font-medium ml-2">
-                          Total Hours:
-                        </h2>
-                        <p className="self-center ml-2">
-                          {chosenUser?.totalHours +
-                            " hour" +
-                            (chosenUser?.totalHours > 1 ? "s" : "")}
-                        </p>
-                      </div>
-                      <div className="flex">
-                        <h2 className="text-lg font-medium ml-2">
-                          Contract Expire Date:
-                        </h2>
-                        <p className="self-center ml-2">
-                          {chosenUser?.endDate?.slice(0, 10)}
-                        </p>
-                      </div>
-                      <div className="flex">
-                        <h2 className="text-lg font-medium ml-2">
-                          Contract BlockChain ID:
-                        </h2>
-                        <p className="self-center ml-2">
-                          {chosenUser?.agreementBlockchainId}
-                        </p>
-                      </div>
-                      <div className="flex">
-                        <h2 className="text-lg font-medium ml-2">
-                          Employee BlockChain ID:
-                        </h2>
-                        <p className="self-center ml-2">
-                          {chosenUser?.userBlockchainId}
-                        </p>
-                      </div>
-                      <div>
-                        <h5 className="ml-2 font-bold mt-2">
-                          Total Wages to be paid
-                        </h5>
-                      </div>
-                      <div>
-                        <CurrencyInput
-                          className="input input-bordered"
-                          prefix="Rp."
-                          defaultValue={
-                            chosenUser?.totalHours * chosenUser?.totalSalary
-                          }
-                          disabled={true}
-                          name="wage"
-                          ref={wage}
-                        />
-                        <button
-                          className="btn"
-                          onClick={(e) => {
-                            wage.current.disabled = false;
-                          }}
-                        >
-                          Edit
-                        </button>
+                      <div className="flex flex-col">
+                        <div className="flex mt-8">
+                          <h2 className="text-md text-gray-500 font-medium ml-2">
+                            Rate Per Hour Fee:
+                          </h2>
+                          <p className="self-center ml-2">
+                            {chosenUser?.Job?.salary.toLocaleString("id-ID", {
+                              style: "currency",
+                              currency: "IDR",
+                            })}
+                          </p>
+                        </div>
+                        <div className="flex">
+                          <h2 className="text-md text-gray-500 font-medium ml-2">
+                            Total Hours:
+                          </h2>
+                          <p className="self-center ml-2">
+                            {chosenUser?.totalHours +
+                              " hour" +
+                              (chosenUser?.totalHours > 1 ? "s" : "")}
+                          </p>
+                        </div>
+                        <div className="flex">
+                          <h2 className="text-md text-gray-500 font-medium ml-2">
+                            Contract Expire Date:
+                          </h2>
+                          <p className="self-center ml-2">
+                            {chosenUser?.endDate?.slice(0, 10)}
+                          </p>
+                        </div>
+                        <div className="flex">
+                          <h2 className="text-md text-gray-500 font-medium ml-2">
+                            Contract BlockChain ID:
+                          </h2>
+                          <p className="self-center ml-2">
+                            {chosenUser?.agreementBlockchainId}
+                          </p>
+                        </div>
+                        <div className="flex">
+                          <h2 className="text-md text-gray-500 font-medium ml-2">
+                            Employee BlockChain ID:
+                          </h2>
+                          <p className="self-center ml-2">
+                            {chosenUser?.userBlockchainId}
+                          </p>
+                        </div>
+                        <div className="mt-8">
+                          <h5 className="ml-2 font-bold mt-2">
+                            Total Wages to be paid
+                          </h5>
+                        </div>
+                        <div className="flex">
+                          <CurrencyInput
+                            className="input input-bordered w-3/4"
+                            prefix="Rp."
+                            defaultValue={
+                              chosenUser?.totalHours * chosenUser?.totalSalary
+                            }
+                            disabled={true}
+                            name="wage"
+                            ref={wage}
+                          />
+                          <button
+                            className="btn w-1/4"
+                            onClick={(e) => {
+                              wage.current.disabled = false;
+                            }}
+                          >
+                            Edit
+                          </button>
+                        </div>
                       </div>
                     </div>
                     {/* <p>{JSON.stringify(chosenUser)}</p> */}
                   </div>
 
-                  <div className="mt-4 flex justify-between">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      Back
-                    </button>
+                  <div className="mt-4 flex justify-end">
                     <button
                       type="button"
                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
