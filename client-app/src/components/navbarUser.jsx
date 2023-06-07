93ba5474aa6a03c9b6ffc7fd33d66e25c5cedf3d
drestwn <andremakmursetaiwan@gmail.com>
2023-06-07 23:20:54 +0700

    feat:update

--- client-app/src/components/navbarUser.jsx
+++ client-app/src/components/navbarUser.jsx
@@ -18,6 +18,7 @@ const NavBarUser = () => {
 
   const base_url_server = "http://localhost:3000";
   const [balance, setBalance] = useState();
+  const [imgProfile, setimgProfile] = useState();
 
   function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
@@ -38,11 +39,13 @@ const NavBarUser = () => {
         style: "currency",
         currency: "IDR",
       });
+      setimgProfile(response.data.User.imgUrl);
       setBalance(formattedAmount);
     } catch (err) {
       console.log(err);
     }
   };
+
   const signOut = () => {
     localStorage.clear();
     router.push("/");
@@ -157,7 +160,7 @@ const NavBarUser = () => {
                           <span className="sr-only">Open user menu</span>
                           <img
                             className="h-8 w-8 rounded-full"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            src={imgProfile}
                             alt=""
                           />
                         </Menu.Button>
