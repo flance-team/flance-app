4a0d0f656949e9c94daaf4f114d8283aaee2eabe
akangRio <fachryo97@gmail.com>
2023-06-08 02:25:32 +0700

    gege

--- client-app/src/components/NavbarEmployer.jsx
+++ client-app/src/components/NavbarEmployer.jsx
@@ -103,7 +103,7 @@ const NavbarEmployer = () => {
                         onClick={() => router.push("/EmployerListEmployee")}
                         className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                       >
-                        List Employee
+                        Employee List
                       </a>
                     </Link>
 
