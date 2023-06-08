cb0dfafc36ffe058ac666496e055ad3d007c3dc0
Septian Maulana <septianm028@gmail.com>
2023-06-08 09:58:33 +0700

    fix: fixed images for primary features

--- client-app/src/components/PrimaryFeatures.jsx
+++ client-app/src/components/PrimaryFeatures.jsx
@@ -16,24 +16,24 @@ const features = [
     title: "Easily Post New Position",
     description:
       "Keep track of everyone's salaries and whether or not they've been paid. Direct deposit not supported.",
-    image: "https://firebasestorage.googleapis.com/v0/b/hacktiv8-tb.appspot.com/o/Screenshot%202023-06-08%20at%2009.10.39.png?alt=media&token=824a14a9-0c7d-42b6-ae88-e6547c454d35",
+    image: "https://firebasestorage.googleapis.com/v0/b/hacktiv8-tb.appspot.com/o/Screenshot%202023-06-08%20at%2009.41.27.png?alt=media&token=fab7fe94-2322-4edb-bdfe-74a4912c0898",
   },
   {
     title: "Accepting Employee",
     description:
       "Empowers employers to efficiently identify the most suitable candidates for their job openings",
-    image: "https://firebasestorage.googleapis.com/v0/b/hacktiv8-tb.appspot.com/o/Screenshot%202023-06-08%20at%2009.09.13.png?alt=media&token=ad27ac3b-43a7-4b94-a885-7d0edd943074",
+    image: "https://firebasestorage.googleapis.com/v0/b/hacktiv8-tb.appspot.com/o/Screenshot%202023-06-08%20at%2009.39.02.png?alt=media&token=d6a5e2f1-c19b-42d5-bc15-b0da2c0897a3",
   },
   {
     title: "Discover Jobs",
     description: "Facilitates an enhanced job search experience for users.",
-    image: "https://firebasestorage.googleapis.com/v0/b/hacktiv8-tb.appspot.com/o/Screenshot%202023-06-08%20at%2009.05.37.png?alt=media&token=ec096bab-5353-4dec-82c5-9359f7e12dc4",
+    image: "https://firebasestorage.googleapis.com/v0/b/hacktiv8-tb.appspot.com/o/Screenshot%202023-06-08%20at%2009.34.49.png?alt=media&token=1f263595-5874-4279-a020-89090482b081",
   },
   {
     title: "Deposit Money & Withdraw the wages",
     description:
       "Easily pay the wages and maintain good credibility of the business. Keep track of everyone's salaries and whether or not they've been paid.",
-    image: "https://firebasestorage.googleapis.com/v0/b/hacktiv8-tb.appspot.com/o/Screenshot%202023-06-08%20at%2008.59.35.png?alt=media&token=18ee9828-7645-4f8c-8673-065f146be228",
+    image: "https://firebasestorage.googleapis.com/v0/b/hacktiv8-tb.appspot.com/o/Screenshot%202023-06-08%20at%2009.30.18.png?alt=media&token=db06e93b-c2f9-45fd-bdf9-c7e108de736a",
   },
 ];
 
@@ -135,7 +135,7 @@ export function PrimaryFeatures() {
                       </p>
                     </div>
                     <div className="mt-10 w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-blue-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
-                      <Image
+                      <img
                         className="w-full"
                         src={feature.image}
                         alt=""
